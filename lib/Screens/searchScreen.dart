8a921e1c4f6633cbeaa82d97c92da1759f6f64da
Mahmoud Mahajna <mahmoud.mahajna24@gmail.com>
2022-05-10 13:45:21 +0300

    search done only locally #31

--- lib/Screens/searchScreen.dart
+++ lib/Screens/searchScreen.dart
@@ -1,5 +1,9 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:yourfitnessguide/utils/constants.dart';
+import 'package:yourfitnessguide/utils/users.dart';
+
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -12,12 +16,60 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Search'),
-      ),
-      body: const Center(child: Text('Search'))
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Search'),
+          actions: [
+            Padding(
+                padding: const EdgeInsets.only(right: 12.0),
+                child: Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/edit');
+                        },
+                        icon: Icon(
+                          Icons.edit,
+                          color: Colors.white,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          user.signOut();
+                          Navigator.pushReplacementNamed(
+                              context, homeRoute);
+                        },
+                        icon: Icon(
+                          Icons.logout,
+                          color: Colors.white,
+                        ))
+                  ],
+                )),
+          ],
+        ),
+      body: Container(),
     );
   }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lib/utils/book_data.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'package:yourfitnessguide/Screens/profileScreen.dart';
+import 'package:yourfitnessguide/Screens/BlogPostScreen.dart';
+import 'package:yourfitnessguide/Screens/signinScreen.dart';
+import 'package:yourfitnessguide/Screens/signupScreen.dart';
+import 'package:yourfitnessguide/Screens/resetPasswordScreen.dart';
+import 'package:yourfitnessguide/Screens/editProfileScreen.dart';
+import 'package:yourfitnessguide/home.dart';
+import 'package:yourfitnessguide/utils/constants.dart';
+import 'package:yourfitnessguide/Screens/workoutScreen.dart';
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case loginRoute:
+      return MaterialPageRoute(builder: (context) => LoginScreen());
+    case signupRoute:
+      return MaterialPageRoute(builder: (context) => SignupScreen());
+    case resetPasswordRoute:
+      return MaterialPageRoute(builder: (context) => ResetPasswordScreen());
+    case homeRoute:
+      return MaterialPageRoute(builder: (context) => HomeScreen());
+    case blogPostRoute:
+      return MaterialPageRoute(builder: (context) => BlogPostScreen());
+    case workoutPostRoute:
+      return MaterialPageRoute(builder: (context) => WorkoutScreen());
+    case editProfileRoute:
+      return MaterialPageRoute(builder: (context) => EditProfileScreen());
+    case profileRoute:
+      return MaterialPageRoute(builder: (context) => ProfileScreen());
+    default:
+      return MaterialPageRoute(builder: (context) => HomeScreen());
+  }
+}
