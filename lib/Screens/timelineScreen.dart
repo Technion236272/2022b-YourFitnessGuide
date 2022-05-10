766166b5a33d7167bd6643a1c218f20bbc4395c1
SalehKa <69247299+SalehKa@users.noreply.github.com>
2022-05-10 13:25:26 +0300

    Encapsulating Post + Adding two 2 more posts

--- lib/Screens/timelineScreen.dart
+++ lib/Screens/timelineScreen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
+import 'package:yourfitnessguide/posts/mypost_1.dart';
+import 'package:yourfitnessguide/posts/mypost_2.dart';
+import 'package:yourfitnessguide/posts/mypost_3.dart';
+
 class TimelineScreen extends StatefulWidget {
   const TimelineScreen({Key? key}) : super(key: key);
 
@@ -57,92 +61,9 @@ class _TimelineScreenState extends State<TimelineScreen> {
         controller: _controller,
         scrollDirection: Axis.vertical,
         children: [
-          Card(
-            elevation: 5,
-            color: Theme.of(context).cardColor,
-            margin: const EdgeInsets.all(16),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ListTile(
-                    contentPadding: const EdgeInsets.all(0),
-                    leading: CircleAvatar(
-                      radius: 30,
-                      backgroundImage:
-                        Image.asset('images/decorations/mclovin.png').image //picture
-                    ),
-                    title: Text('McLovin',
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText1!
-                            .copyWith(fontSize: 18, fontWeight: FontWeight.w600)),
-                    trailing: IconButton(
-                        onPressed: null,//todo
-                        icon: Icon(
-                          Icons.more_horiz,
-                          color:
-                          Theme.of(context).iconTheme.color,
-                        )),
-                    ),
-                    const Text( 'Jesus died for our sins, Harambe died for our memes. #rip #legend #wow',
-                        textAlign: TextAlign.left),
-
-                      Column(
-                        crossAxisAlignment:  CrossAxisAlignment.start,
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.asset(
-                              'images/posts/harambe.jpg',
-                              height: 200,
-                              width: MediaQuery.of(context).size.width,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: const [
-                                  IconButton(
-                                      onPressed: null,
-                                      icon: Icon(
-                                          Icons.arrow_upward,
-                                          color: Colors.grey
-                                      )
-                                  ),
-                                  IconButton(
-                                      onPressed: null,
-                                      icon: Icon(
-                                          Icons.arrow_downward,//.comment_lines,
-                                          color: Colors.grey
-                                      )
-                                  )
-                                ],
-                              ),
-                              const IconButton(
-                                onPressed: null,
-                                icon: Icon(
-                                  Icons.chat_bubble,
-                                  color: Colors.grey
-                                )
-                              ),
-                              const IconButton(
-                                  onPressed: null,
-                                  icon: Icon(
-                                    Icons.bookmark,
-                                    color: Colors.grey,
-                                  ))
-                            ],
-                          ),
-                        ],
-                      ),
-                ]
-              )
-            )
-          )
+          MyPost1(),
+          MyPost2(),
+          MyPost3(),
         ],
       ),
     );
